3a4ed460c80aa4f808a491cb729d8b32b3f2c1b0
Justin Byrd <justinbyrd7@gmail.com>
2024-01-18 15:27:56 -0600

    Update ProjectsPage component and App.css

--- src/components/ProjectsPage.jsx
+++ src/components/ProjectsPage.jsx
@@ -4,16 +4,17 @@ export default function ProjectsPage() {
   return (
     <div className="projects-page">
       <Header />
-      <div className="project-container">
-        {" "}
-        <h2>Project 1 Name</h2>
-        <p>Image of Project here</p>
-        <div className="code-links">
-          {" "}
-          <p>Code</p>
-          <p>Live</p>
-        </div>
-      </div>
+      <h1>Coming Soon...</h1>
+      <div className="project-container"> </div>
     </div>
   );
+}
+{
+  /* <h2>Project 1 Name</h2>
+<p>Image of Project here</p>
+<div className="code-links">
+  {" "}
+  <p>Code</p>
+  <p>Live</p>
+</div> */
 }
