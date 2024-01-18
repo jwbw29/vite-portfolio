c517c2db7c566676d312a0eb92b952cc6e5a37ce
Justin Byrd <justinbyrd7@gmail.com>
2024-01-18 16:55:43 -0600

    Add project cards to ProjectsPage

--- src/components/ProjectsPage.jsx
+++ src/components/ProjectsPage.jsx
@@ -1,20 +1,51 @@
 import Header from "./Header";
+import "../styles/ProjectsPage.css";
 
 export default function ProjectsPage() {
   return (
     <div className="projects-page">
       <Header />
-      <h1>Coming Soon...</h1>
-      <div className="project-container"> </div>
+      <div className="project-container">
+        <div className="single-project">
+          <h2>Asylum Office Grant Rate Tracker</h2>
+          <p>Image of Project here</p>
+          <div className="code-links">
+            {" "}
+            <p>Code</p>
+            <p>Live</p>
+          </div>
+        </div>
+
+        <div className="single-project">
+          <h2>Asylum Office Grant Rate Tracker</h2>
+          <p>Image of Project here</p>
+          <div className="code-links">
+            {" "}
+            <p>Code</p>
+            <p>Live</p>
+          </div>
+        </div>
+
+        <div className="single-project">
+          <h2>Project 3</h2>
+          <p>Image of Project here</p>
+          <div className="code-links">
+            {" "}
+            <p>Code</p>
+            <p>Live</p>
+          </div>
+        </div>
+
+        <div className="single-project">
+          <h2>Project 4</h2>
+          <p>Image of Project here</p>
+          <div className="code-links">
+            {" "}
+            <p>Code</p>
+            <p>Live</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
-{
-  /* <h2>Project 1 Name</h2>
-<p>Image of Project here</p>
-<div className="code-links">
-  {" "}
-  <p>Code</p>
-  <p>Live</p>
-</div> */
 }
