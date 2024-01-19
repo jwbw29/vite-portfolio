0f43301c333e805c11c02c77e9fba236522262b3
Justin Byrd <justinbyrd7@gmail.com>
2024-01-19 10:40:49 -0600

    wip projects css

--- src/components/ProjectsPage.jsx
+++ src/components/ProjectsPage.jsx
@@ -5,44 +5,46 @@ export default function ProjectsPage() {
   return (
     <div className="projects-page">
       <Header />
-      <div className="project-container">
-        <div className="single-project">
-          <h2>Pizza Form</h2>
-          <p>Image of Project here</p>
-          <div className="code-links">
-            {" "}
-            <p>Code</p>
-            <p>Live</p>
+      <div className="main">
+        <div className="project-container">
+          <div className="single-project">
+            <h2>Pizza Form</h2>
+            <p>Image of Project here</p>
+            <div className="code-links">
+              {" "}
+              <p>Code</p>
+              <p>Live</p>
+            </div>
           </div>
-        </div>
 
-        <div className="single-project">
-          <h2>Asylum Office Grant Rate Tracker</h2>
-          <p>Image of Project here</p>
-          <div className="code-links">
-            {" "}
-            <p>Code</p>
-            <p>Live</p>
+          <div className="single-project">
+            <h2>Asylum Office Grant Rate Tracker</h2>
+            <p>Image of Project here</p>
+            <div className="code-links">
+              {" "}
+              <p>Code</p>
+              <p>Live</p>
+            </div>
           </div>
-        </div>
 
-        <div className="single-project">
-          <h2>RoboFriends</h2>
-          <p>Image of Project here</p>
-          <div className="code-links">
-            {" "}
-            <p>Code</p>
-            <p>Live</p>
+          <div className="single-project">
+            <h2>RoboFriends</h2>
+            <p>Image of Project here</p>
+            <div className="code-links">
+              {" "}
+              <p>Code</p>
+              <p>Live</p>
+            </div>
           </div>
-        </div>
 
-        <div className="single-project">
-          <h2>User Onboarding</h2>
-          <p>Image of Project here</p>
-          <div className="code-links">
-            {" "}
-            <p>Code</p>
-            <p>Live</p>
+          <div className="single-project">
+            <h2>User Onboarding</h2>
+            <p>Image of Project here</p>
+            <div className="code-links">
+              {" "}
+              <p>Code</p>
+              <p>Live</p>
+            </div>
           </div>
         </div>
       </div>
