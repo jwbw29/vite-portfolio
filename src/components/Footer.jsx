cec84152802611617aa4fdeba9791043f4b2bb6d
Justin Byrd <justinbyrd7@gmail.com>
2024-02-13 17:37:40 -0600

    wip

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,9 @@ const dateUpdated = "02-13-2024";
 export default function Footer() {
   return (
     <div className="footer">
-      Last Updated: <span id="dateUpdated"> {dateUpdated}</span>{" "}
+      <p>
+        Last Updated: <span id="dateUpdated">{dateUpdated}</span>{" "}
+      </p>
     </div>
   );
 }
